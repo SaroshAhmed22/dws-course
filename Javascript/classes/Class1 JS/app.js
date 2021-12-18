999185b4f4adb9cbbdd36ea08a3410ec559d34ee
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2021-12-18 18:54:38 +0500

    Sunday class sections

---
+++ Javascript/classes/Class1 JS/app.js
@@ -0,0 +1,90 @@
+// console.log("Hello World")
+// console.log("Welcome to JS Course")
+// alert("My name is Muzammil")
+
+// var Myname = "Muzammil";
+// console.log(Myname);
+// var my_age = 21;
+// console.log(my_age)
+// console.log(typeof (Myname))
+
+// var logic = 2 + 3 + 4 + 4 + 5 + 5 + 5 + 5 + 5 + 56 + 6 + 6 + 6 + 6 + 6 + 6 + 6 + 5 + 5 + 34 + 34 + 34 + 3;// console.log(logic);
+// var iamsarosh = true;
+// console.log(iamsarosh);
+
+// console.log(10 + "20");
+// 9 - "5"
+// console.log("Java" + "Script");
+//bug
+// "Java" + "Script"
+// console.log( "Java "+ "Script");
+// " " + " "
+// console.log( " " + 0);
+// " " + 0
+// "sarosh" - "Ahmed"
+// true + true
+// true + false
+// false + true
+// false - true
+// console.log((true + false + false + true + true + true) * false)
+// var mylastname = null
+// console.log(mylastname)
+// var iAmUseless = null;
+// console.log(typeof (iAmUseless))
+// / var iAmStandBy;
+// console.log(iAmStandBy);
+// console.log(typeof (iAmStandBy));
+// console.log("ab" + "ab")
+// var myPhoneNumber = 9876543210;
+// var myName = "Sarosh";
+// console.log(isNaN(myPhoneNumber));
+// console.log(isNaN(myName));
+
+// var myfname = "Sarosh";
+// var mylname = "Ahmed"
+// var age = "21";
+// console.log(" My First Name is " + myfname)
+// console.log(" My last Name is " + mylname)
+// console.log("My age is " + age)
+
+// var a = '10';
+// var b = '20';
+// console.log(a)
+// console.log(b)
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a = b)
+
+
+// console.log(a === b)
+// console.log(40 % 2/)
+
+
+// var x = 12;
+// console.log(x--)
+// console.log(x)
+
+
+// var num = 3;
+// var num2 = 3;
+// var newNum = --num + 5;
+// console.log(num)
+// console.log(newNum);
+// console.log(++num - num2--)var 
+
+
+// console.log(num != num2)
+
+// var a = 10;
+// var b = 20;
+
+// console.log(a <= b)
+
+var Test1 = 55;
+var Test2 = 20;
+console.log(Test1 >= 50 || Test2 >= 50)
+
+
+// console.log(a > b && b > -50 && b < 0);
