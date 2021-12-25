db19731b5721b48a480ea9d3226d8198388979f3
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2021-12-25 14:54:20 +0500

    class3

---
+++ Javascript/classes/class2/app.js
@@ -0,0 +1,192 @@
+console.log("CLass 2 ")
+// var num = 40;
+
+// if (num >= 50) {
+//     console.log("Pass")
+// }
+// else {
+//     console.log("Fail")
+// }
+
+// var tomr = 'rain';
+
+// if (tomr == 'rain') {
+//     console.log('take a raincoat');
+// } else {
+//     console.log('No need to take a raincoat');
+// }
+
+
+// var age = 17;
+// if (age >= 18) {
+//     console.log("you are eligible to vote");
+// } else {
+//     console.log("you are not eligible to vote");
+// }
+
+
+
+
+// var area = "triangle";
+// var PI = 3.142, l = 5, b = 4, r = 3;
+
+// if (area == "circle") {
+//     console.log("the area of the circle is : " + PI * r ** 2);
+// }
+// else if (area == "triangle") {
+//     console.log("the area of the triangle is : " + (l * b) / 2);
+// }
+// else if (area == "rectangle") {
+//     console.log("the area of the rectangle is : " + (l * b));
+// }
+// else {
+//     console.log("please enter valid data");
+// }
+
+
+// var area = "rectangle";
+// var PI = 3.142, l = 5, b = 4, r = 3;
+
+// switch (area) {
+//     case 'circle':
+//         console.log("the area of the circle is : " + PI * r ** 2);
+//         break;
+
+//     case 'triangle':
+//         console.log("the area of the triangle is : " + (l * b) / 2);
+//         break;
+
+//     case 'rectangle':
+//         console.log("the area of the rectangle is : " + (l * b));
+//         break;
+
+//     default:
+//         console.log("please enter valid data");
+// }
+
+
+
+
+
+// var num = 0;
+// while (num <= 20) {
+//     console.log("Hellow")
+//     num++;
+// }
+
+
+// console.log("CLass 2 ")
+
+// var num = 0;
+// do {
+//     console.log(num);
+//     num++;
+// } while (num <= 10);
+
+// for (var num = 0; num <= 100; num++) {
+//     console.log(num);
+// }
+
+// for (var num = 1; num <= 10; num++) {
+//     var tableOf = 8;
+//     console.log(tableOf + " * " + num + " = " + tableOf * num);
+// }
+// var a = 10;
+// var b = 20;
+// var sum = a + b;
+// console.log(sum);
+// var c = 100;
+// var d = 60;
+// var sum2 = c + d;
+// console.log(sum2);
+
+
+
+// function sum(a, b) {
+//     var total = a / b;
+//     console.log(total);
+// }
+
+// sum();
+// sum(2, 3);
+// sum(50, 50);
+// sum(5, 6)
+// sum(6, 6)
+// sum(9, 6)
+// sum(7, 7)
+// sum(6, 8)
+// sum(56, 7)
+
+// function sum(a, b) {
+//     var total = a + b;
+//     console.log(total);
+// }
+
+// var funExp2 = sum(5, 15);
+// console.log(funExp2)
+// function sum(a, b) {
+//     return total = a + b;
+// }
+// var funExp = sum(5, 25);
+// var funExp3 = sum(4, 7);
+// console.log('the sum of two no is ' + funExp);
+// console.log('the sum of two no is ' + funExp3);
+// console.log(typeof (funExp))
+
+// function table(t) {
+//     return (
+//     for (var num = 1; num <= 10; num++) {
+//     var tableOf = 8;
+//     console.log(tableOf + " * " + num + " = " + tableOf * num);
+// }
+
+//     )
+// }
+
+
+
+// function tableOf(t) {
+//     for (var num = 1; num <= 10; num++) {
+//         console.log(t + " * " + num + " = " + t * num);
+//     }
+// }
+// tableOf(5)
+// console.log("")
+// tableOf(6)
+// console.log("")
+
+// tableOf(9)
+
+
+// {
+//     let x = 2;
+//     console.log(x)
+
+// }
+
+
+
+
+// function under() {
+//     var z = 19;
+//     console.log(z)
+// }
+// carName = "Saab";
+// let carName = "Volvo";
+// console.log(carName)
+
+
+
+// {
+//     const a = 12;
+//     console.log(a)
+// }
+
+// console.log(a)
+
+
+var myname = "Sarosh"
+console.log(myname)
+
+myname = "Kahyan"
+console.log(myname)
---
+++ Javascript/classes/class3/index.js
@@ -0,0 +1,81 @@
+console.log("Class 3")
+//  Fat arrow Function
+// const myFun = () => {
+//     console.log("I am a Function")
+// }
+// myFun();
+// const car = "Corolla "
+// const car2 = "civic "
+// const cars = ["audi", "Saab", "Volvo", "BMW", "Civic"];
+// console.log(cars[0])
+// let Fruit = ["Apple", "Mango", "Banana"]
+// Fruit.unshift("PineApple")
+// console.log(Fruit)
+// Fruit.shift();
+// console.log(Fruit)
+// let Fruit = ["Apple", "Mango", "Banana"]
+// Fruit.forEach((elem, index) => {
+//     console.log(index, elem)
+// })
+// console.log(Fruit)
+// Fruit.map((elem, index) => {
+//     console.log(elem);// })
+// let txt = " Sarosh";
+// let length = txt.length;
+// console.log(length)
+// let str = "Apple, Banana, Kiwi";
+// let part = str.slice(7, 14);
+// let part = str.lastIndexOf("i");
+// console.log(part)
+// let email = "SaroshAhmedkhanzadaAHmed@gmail.com"
+// let str = email.indexOf("@");
+// console.log(str)
+// let myname = email.substring(0, str)
+// console.log(myname)
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText)
+// let Ourname = "Mohammad Kahyan";
+// let myname = Ourname.replace("Kahyan", "Ahmed");
+// console.log(myname)
+// let fname = "Sarosh"
+// let lname = "Ahmed"
+// let myname = fname.concat(" ", lname)
+// console.log(myname)
+// let text = "HELLO WORLD";
+// let char = text.charAt(1);
+// console.log(char)
+// let fname = "Sarosh"
+// let lname = "Ahmed"
+// console.log("My First name is " + fname, "My last name is " + lname)
+// console.log(` My First name is ${fname}  My last name is ${lname}`)
+// const Std1 = {
+//     Fullname: "Kahyan",
+//     Course: "React",
+//     Hobby: "Coding",
+// }
+// console.log(Std1.Fullname)
+// console.log(Std1.Course)
+// console.log(Std1.Hobby)
+// 
+
+const myevent = () => {
+    // let heading = document.getElementById("ourID")
+    // heading.style.backgroundColor = "blue";
+    // heading.style.textAlign = "center";
+    // console.log(heading)
+    // heading.innerText = "I am Changed"
+    // let ourbody = document.getElementById("ourbody")
+    // console.log(ourbody)
+    // ourbody.style.backgroundColor = "red";
+
+    let mydisplay = document.getElementById("mydisplay")
+
+    console.log(mydisplay)
+
+    mydisplay.style.display = "flex"
+    mydisplay.style.justifyContent = "space-around"
+
+
+
+}
