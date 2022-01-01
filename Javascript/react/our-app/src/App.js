517d7c403171236a9f1664efa3f7532bc39478c1
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2022-01-01 15:18:49 +0500

    Update App.js

--- Javascript/react/our-app/src/App.js
+++ Javascript/react/our-app/src/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import { React, useState } from "react";
 import Component1 from './Component1'
 import Header from './Components/Header'
 import Footer from './Components/Footer';
 
 const App = () => {
+
+  const [count, setcount] = useState(0)
+
+  const inc = () => {
+    console.log("I am Called")
+    setcount(count + 1)
+
+  }
   return (
     <div>
-      <Header myname="Sarosh" />
-      <Component1 school="kps" />
-      <Footer />
+      <h1>{count}</h1>
+      <button onClick={inc}>Increment</button>
     </div>
   )
 }
