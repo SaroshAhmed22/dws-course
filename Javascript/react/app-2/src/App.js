08120da6ee4ae86fe515834a214ef88e21f21bac
SaroshAhmed22 <SaroshAhmed622@gmail.com>
2022-01-02 14:49:11 +0500

    React class 2 end

--- Javascript/react/app-2/src/App.js
+++ Javascript/react/app-2/src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import bikeImage from './img1.jfif'
+// import bikeImage from './img1.jfif'
+import Menu from './menuapi';
 
 function App() {
+  console.log(Menu)
 
 
   return (
-    <div >
-      <h1 className="text-primary">I am text</h1>
+    <div>
+      {
+        Menu.map((elem, index) => {
+          const { id, image, name, category, price, description } = elem;
+          return (
+            <div className="mt-5 col-md-4 col-12">
+              <div className="card " >
+                <img src={image} height="300px" className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{category}</h5>
+                  <h5 className="card-title">{name}</h5>
+                  <p className="card-text">{description}</p>
+                  <p className="card-text">{price}</p>
+                </div>
 
-
-
-
-
-
-
+              </div>
+            </div>
+          )
+        })
+      }
     </div >
   );
 }
